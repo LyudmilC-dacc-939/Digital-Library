71cc14929ab6691bc7d734c1454ab4160e3060ae
LAC_Knightmare <Whats_with_lusk0@abv.bg>
2024-07-17 17:52:57 +0300

    added endpoints

--- src/api/GeneralSearchResponse.java
+++ src/api/GeneralSearchResponse.java
@@ -1,88 +1,73 @@
 package api;
 
+import java.util.List;
+
 public class GeneralSearchResponse {
     private Integer numFound;
     private Integer start;
-    private boolean numFoundExact;
-    private Docs docs;
+    private Boolean numFoundExact;
+    private List<Docs> docs;
     private Integer num_found;
     private String q;
-    private Integer offset;
+    private Integer offset = null;
 
     public GeneralSearchResponse() {
-    }
-
-    public GeneralSearchResponse(Integer numFound, Integer start, boolean numFoundExact, Docs docs, Integer num_found, String q, Integer offset) {
-        this.numFound = numFound;
-        this.start = start;
-        this.numFoundExact = numFoundExact;
-        this.docs = docs;
-        this.num_found = num_found;
-        this.q = q;
-        this.offset = offset;
     }
 
     public Integer getNumFound() {
         return numFound;
     }
 
-    public GeneralSearchResponse setNumFound(Integer numFound) {
+    public void setNumFound(Integer numFound) {
         this.numFound = numFound;
-        return this;
     }
 
     public Integer getStart() {
         return start;
     }
 
-    public GeneralSearchResponse setStart(Integer start) {
+    public void setStart(Integer start) {
         this.start = start;
-        return this;
     }
 
-    public boolean isNumFoundExact() {
+    public Boolean getNumFoundExact() {
         return numFoundExact;
     }
 
-    public GeneralSearchResponse setNumFoundExact(boolean numFoundExact) {
+    public void setNumFoundExact(Boolean numFoundExact) {
         this.numFoundExact = numFoundExact;
-        return this;
     }
 
-    public Docs getDocs() {
+    public List<Docs> getDocs() {
         return docs;
     }
 
-    public GeneralSearchResponse setDocs(Docs docs) {
+    public void setDocs(List<Docs> docs) {
         this.docs = docs;
-        return this;
     }
 
     public Integer getNum_found() {
         return num_found;
     }
 
-    public GeneralSearchResponse setNum_found(Integer num_found) {
+    public void setNum_found(Integer num_found) {
         this.num_found = num_found;
-        return this;
     }
 
     public String getQ() {
         return q;
     }
 
-    public GeneralSearchResponse setQ(String q) {
+    public void setQ(String q) {
         this.q = q;
-        return this;
     }
 
     public Integer getOffset() {
         return offset;
     }
 
-    public GeneralSearchResponse setOffset(Integer offset) {
+    public void setOffset(Integer offset) {
         this.offset = offset;
-        return this;
     }
 }
 

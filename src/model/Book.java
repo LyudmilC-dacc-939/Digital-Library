c682b7f859c57537a1eb6b3248e12e8fe7ecb8d6
LAC_Knightmare <Whats_with_lusk0@abv.bg>
2024-07-18 16:00:18 +0300

    small changes in MainDataProvider

--- src/model/Book.java
+++ src/model/Book.java
@@ -12,4 +12,73 @@ public class Book {
     private Integer num_found;
     private String q;
     private Integer offset = null;
+
+    public Book(Integer numFound, Integer start, Boolean numFoundExact, List<Docs> docs, Integer num_found, String q, Integer offset) {
+        this.numFound = numFound;
+        this.start = start;
+        this.numFoundExact = numFoundExact;
+        this.docs = docs;
+        this.num_found = num_found;
+        this.q = q;
+        this.offset = offset;
+    }
+
+    public Book() {
+    }
+
+    public Integer getNumFound() {
+        return numFound;
+    }
+
+    public void setNumFound(Integer numFound) {
+        this.numFound = numFound;
+    }
+
+    public Integer getStart() {
+        return start;
+    }
+
+    public void setStart(Integer start) {
+        this.start = start;
+    }
+
+    public Boolean getNumFoundExact() {
+        return numFoundExact;
+    }
+
+    public void setNumFoundExact(Boolean numFoundExact) {
+        this.numFoundExact = numFoundExact;
+    }
+
+    public List<Docs> getDocs() {
+        return docs;
+    }
+
+    public void setDocs(List<Docs> docs) {
+        this.docs = docs;
+    }
+
+    public Integer getNum_found() {
+        return num_found;
+    }
+
+    public void setNum_found(Integer num_found) {
+        this.num_found = num_found;
+    }
+
+    public String getQ() {
+        return q;
+    }
+
+    public void setQ(String q) {
+        this.q = q;
+    }
+
+    public Integer getOffset() {
+        return offset;
+    }
+
+    public void setOffset(Integer offset) {
+        this.offset = offset;
+    }
 }
